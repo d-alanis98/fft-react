c0e097cc8ec5a3e74ac836ecdd77f7a88d4f9650
Damian Alanis Ramirez <damian.alanis98@gmail.com>
2020-06-28 02:56:47 -0500

    Deploy

---
+++ precache-manifest.68c16a85e03321d53da5175fd9c5f1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e0b05edaf178a0ffe8ee1644cec82f",
+    "url": "/fft-react/index.html"
+  },
+  {
+    "revision": "11d3d4668ff329a6a3b2",
+    "url": "/fft-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0582b4402cbb6fd0d46d",
+    "url": "/fft-react/static/css/main.3308ef5c.chunk.css"
+  },
+  {
+    "revision": "11d3d4668ff329a6a3b2",
+    "url": "/fft-react/static/js/2.6e1c4979.chunk.js"
+  },
+  {
+    "revision": "169257132f0fccb28d553be249d4e809",
+    "url": "/fft-react/static/js/2.6e1c4979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0582b4402cbb6fd0d46d",
+    "url": "/fft-react/static/js/main.fadf6ecd.chunk.js"
+  },
+  {
+    "revision": "6a761aab2e7c940c5f5d",
+    "url": "/fft-react/static/js/runtime-main.1614657c.js"
+  }
+]);
